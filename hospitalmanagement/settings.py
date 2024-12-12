d57c80af089c633c6bf84020ed4fff7149c70009
nzere.michael <nzeremichael2020@gmail.com>
2024-12-12 19:03:46 +0100

    Added render domain to ALLOWED_HOSTS

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = 'hpbv()ep00boce&o0w7z1h)st148(*m@6@-rk$nn)(n9ojj4c0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://hospitalmanagement-h4ja.onrender.com', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'hospitalmanagement-h4ja.onrender.com']
 
 # Application definition
 
